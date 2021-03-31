2c5fa628ba4ccff50de7c131c7abdcc0d381e8e9
wmerrow <william.merrow@gmail.com>
2021-03-31 00:49:39 -0400

    work pages and homepage bubbles

--- js/chart.js
+++ js/chart.js
@@ -1,3 +1,5 @@
+// create simple bubble plot for homepage
+
 // chart area
 var width = 700;
 var height = 215;
@@ -18,13 +20,12 @@ var svg = d3.select("#chart")
     .attr("preserveAspectRatio","xMidYMid meet");
 
 
-d3.csv("data/parties.csv", function(data_all) {
+d3.csv("data/parties.csv", function(data) {
 
     // DATA
 
-    // filter to last election year
-    var data_le = data_all
-        .filter(({is_last_election}) => is_last_election === "TRUE");
+    // filter to each country's most recent election
+    var data = data.filter(({is_last_election}) => is_last_election === "TRUE");
 
     // SCALES
 
@@ -41,14 +42,18 @@ d3.csv("data/parties.csv", function(data_all) {
         .domain([0, 1])
         .range([margin.top, height - margin.bottom]);
     // color
-    var illibColor = d3.scaleLinear()
-        .domain([1, 0])
-        .range(['#000000','#aaaaaa'])
-        .interpolate(d3.interpolateRgb);
     var immigColor = d3.scaleLinear()
         .domain([-5, 5])
         .range(['red','blue'])
         .interpolate(d3.interpolateRgb);
+    // var illibColor = d3.scaleLinear()
+    //     .domain([1, 0])
+    //     .range(['#000000','#aaaaaa'])
+    //     .interpolate(d3.interpolateRgb);
+
+    // transition durations for two stages of animation
+    var t1 = 2000;
+    var t2 = 2000;
 
     // AXIS LINE
 
@@ -61,26 +66,28 @@ d3.csv("data/parties.csv", function(data_all) {
         .tickValues([])
         .tickSize(0));
 
-    // CIRCLES
-
-    xAxis.transition().duration(2500)
+    xAxis.transition().duration(t1)
         .style('opacity', 1);
+
+    // BUBBLES
 
     var bubbles = svg.append("g").selectAll('circle_fri')
-        .data(data_le.sort(function(a,b) {return d3.descending(+a.v2pavote, +b.v2pavote);})) // sort bubbles so smallest are on top
+        .data(data.sort(function(a,b) {return d3.descending(+a.v2pavote, +b.v2pavote);})) // sort bubbles so smallest are on top
         .enter()
         .append('circle')
-        .attr('cx', d=> bubbleImmigX(+d.v2paimmig))
+        .attr('cx', width / 2)
         .attr('cy', margin.top)
         .attr('r', d=> bubbleSize(+d.v2pavote))
+        .style('fill', '#aaaaaa')
         .style('opacity', 0)
+        .transition().duration(t1)
+        .style('opacity', 1)
         .style('stroke', '#ffffff')
-        .style('stroke-width', '0.5px')
-        .style('fill', d=> immigColor(+d.v2paimmig));
-        // .style('fill', '#aaaaaa');
+        .style('stroke-width', '0.5px');
 
-    bubbles.transition().duration(2000)
-        .style('opacity', 1);
+    bubbles.transition().duration(t2)
+        .attr('cx', d=> bubbleImmigX(+d.v2paimmig))
+        .style('fill', d=> immigColor(+d.v2paimmig));
         // .transition().duration(1250)
         // .attr('cy', d=> bubbleIllibY(+d.v2xpa_illiberal))
         // .style('fill', d=> illibColor(+d.v2xpa_illiberal));
